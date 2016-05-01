fa9334b678eb6a0123a7c7b54fa14c7516205bc8
enoch232 <enoch232@gmail.com>
2016-04-30 22:01:03 -0700

    fix production 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.middleware.use ChatActionCable   
-config.web_socket_server_url = "wss://enochchatapp.herokuapp.com/"
+  config.action_cable.url = 'ws://enochchatapp.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['http://enochchatapp.herokuapp.com']
 end
